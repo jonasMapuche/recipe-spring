6f06b2e664a70ab4a540cb7d2443cb424163f113
jonas mapuche <64318160+jonasMapuche@users.noreply.github.com>
2022-08-03 19:27:50 -0300

    Initial commit

---
+++ src/main/java/br/com/stomach/RecipeSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.stomach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/stomach/RecipeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.stomach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
